f592be35c8b2e6ff22229665ebcf7fee8fc61ba4
Stephen Baidu <stephenbaidu@gmail.com>
2014-04-07 13:30:37 +0000

    Set default value for modules

--- app/assets/angular/app/controllers/main_controller.js
+++ app/assets/angular/app/controllers/main_controller.js
@@ -1,9 +1,9 @@
 angular.module("app").controller("MainController", ['$rootScope', 'APP', '$state', '$stateParams',
   function ($rootScope, APP, $state, $stateParams) {
-  // window.MainController = $rootScope;
+  window.MainController = $rootScope;
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
-  $rootScope.modules = APP['modules'];
+  $rootScope.modules = APP['modules'] || [];
 
   $rootScope.hasUrl = function(url) {
     return window.location.hash.indexOf(url) == 0;
