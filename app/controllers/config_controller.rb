d8ed5fdaf99d28220f0ee46df02bb965334b9da0
Stephen Baidu <stephenbaidu@gmail.com>
2015-05-25 12:56:38 +0000

    Updated config controller

--- app/controllers/config_controller.rb
+++ app/controllers/config_controller.rb
@@ -63,14 +63,19 @@ class ConfigController < ApplicationController
           type:  col.type.to_s
         }
 
-        if col.type == :date
+        if col.type == :date || col.type == :datetime
           prop[:type] = 'string'
           prop[:format] = 'date'
         end
 
         prop[:type] = 'string' if col.type == :text
-        prop[:type] = 'string' if col.type == :decimal
+        prop[:type] = 'number' if col.type == :decimal
         prop[:type] = 'number' if col.type == :integer
+
+        if col.name == 'email' || col.name.ends_with?('_email')
+          prop[:pattern] = '^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$'
+          prop[:validationMessage] = 'Should be a correct Email Address'
+        end
 
         # Is it a lookup
         if foreign_keys.keys.include?(col.name)
@@ -90,11 +95,25 @@ class ConfigController < ApplicationController
           begin
             tag_model_class = "#{@model_class.name}Tag".constantize
             prop[:type] = 'array'
+            prop[:default] = []
             prop[:format] = 'uiselect'
-            prop[:placeholder] = "Select tags ..."
+            prop[:placeholder] = 'Select tags ...'
             prop[:items] = tag_model_class.all.map { |e| {value: e.id, label: e.name} }
           rescue Exception => e
             
+          end
+        elsif col.name.ends_with? '_ids'
+          begin
+            title = col.name[0, col.name.length - 4].classify
+            tag_model_class = title.constantize
+            prop[:title] = title.titleize.pluralize
+            prop[:type] = 'array'
+            prop[:default] = []
+            prop[:format] = 'uiselect'
+            prop[:placeholder] = "Select #{title.pluralize.underscore} ..."
+            prop[:items] = tag_model_class.all.map { |e| {value: e.id, label: e.name} }
+          rescue Exception => e
+            pp e
           end
         end
 
@@ -150,6 +169,14 @@ class ConfigController < ApplicationController
             selectYears: true,
             selectMonths: true
           }
+        elsif col.type == :datetime
+          field[:type] = "datepicker"
+          # field[:format] = "yyyy-mm-dd"
+          field[:pickadate] = {
+            selectYears: true,
+            selectMonths: true,
+            format: "yyyy-mm-dd"
+          }
         end
 
         # Is it a lookup
@@ -168,7 +195,7 @@ class ConfigController < ApplicationController
 
         section[:items] << {
           type: "section",
-          htmlClass: "col-xs-6",
+          htmlClass: "col-sm-6 col-xs-12",
           items: [field]
         }
       end
@@ -186,7 +213,10 @@ class ConfigController < ApplicationController
   private
     def excluded_field?(field)
       # ["id", "created_at", "updated_at"].include? field
-      ['id', 'created_at', 'updated_at', 'details', 'lft', 'rgt', 'depth'].include? field
+      [
+        'id', 'created_at', 'updated_at', 'details',
+        'lft', 'rgt', 'depth'
+      ].include? field
     end
 
     def is_nested_set
