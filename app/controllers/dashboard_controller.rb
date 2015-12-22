1a744f0e85299758957e5f53d25a5d5b679ed738
Stephen Baidu <stephenbaidu@gmail.com>
2015-12-22 00:09:24 +0000

    Data for main "default" dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -30,6 +30,21 @@ class DashboardController < ApplicationController
     render json: data
   end
 
+  def default
+    data = {}
+    data[:labels] = ["January", "February", "March", "April", "May", "June", "July"];
+    data[:series] = ['Series A', 'Series B'];
+    data[:data] = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ]
+    
+    data[:labels2] = ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales"];
+    data[:data2] = [300, 100, 40, 120];
+
+    render json: data
+  end
+
   private
     def authorize_data
       true
