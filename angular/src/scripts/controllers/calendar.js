65112f2975f4c38e41fb2f8883c44a5adb5d5234
Stephen Baidu <stephenbaidu@gmail.com>
2016-01-13 00:00:19 +0000

    Calendar controller

---
+++ angular/src/scripts/controllers/calendar.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:CalendarCtrl
+ * @description
+ * # CalendarCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('CalendarCtrl', function (APP, $http) {
+    var vm = this;
+
+    vm.routeName = null;
+
+    // Initial calender values
+    vm.view = 'month';
+    vm.events = [];
+    vm.viewDate = new Date();
+    vm.viewTitle = '';
+
+    vm.initRoute = function (routeName, options) {
+      vm.routeName = routeName;
+      options = options || {};
+
+      if (options.view) {
+        vm.view = options.view;
+      }
+      
+      vm.reload();
+    }
+
+    vm.reload = function () {
+      $http.get(APP.apiPrefix + 'dashboard/' + vm.routeName)
+        .success(function (data) {
+          vm.events = _.map(data, function (e) {
+            // e.startsAt = moment(e.startsAt).toDate();
+            // e.endsAt = moment(e.endsAt).toDate();
+            e.incrementsBadgeTotal = false;
+            return e;
+          });
+        });
+    }
+  });
